3fe2c6ed3d4e4f278d59fe77f3af3203a25d6d04
RussellPaine <russell.d.paine@gmail.com>
2020-06-08 22:32:21 +0100

    The start of the project

---
+++ main.py
@@ -0,0 +1,16 @@
+"""Simple program to figure out the
+    best and easiest circuit to make
+"""
+def main():
+    """Main Function to read in the
+        users values
+    """
+    num_circ = input("Num Of Circuits: ")
+    print("1: LV 2: MV 3: HV 4: EV 5: IV")
+    lvl_circ = input("Level Of Circuits: ")
+    print("1: Basic 2: Good 3: Plastic 4: Advanced")
+    curr_lvl = input("Level Of Board: ")
+
+
+if __name__ == "__main__":
+    main()
